24b7906e8fceda8160d6aa194829b8117e8fabb0
berluk12@gmail.com <berluk12@gmail.com>
2025-01-03 19:55:42 +0100

    Added base service as an abstract class

--- src/main/java/com/bervan/streamingapp/CanvasService.java
+++ src/main/java/com/bervan/streamingapp/CanvasService.java
@@ -1,10 +1,9 @@
 package com.bervan.streamingapp;
 
+import com.bervan.common.search.SearchService;
 import com.bervan.common.service.AuthService;
 import com.bervan.common.service.BaseService;
-import com.bervan.common.user.User;
 import com.bervan.core.model.BervanLogger;
-import com.bervan.ieentities.ExcelIEEntity;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +13,16 @@ import java.util.Set;
 import java.util.UUID;
 
 @Service
-public class CanvasService implements BaseService<UUID, Canvas> {
+public class CanvasService extends BaseService<UUID, Canvas> {
     private final CanvasRepository repository;
     private final HistoryCanvasRepository historyRepository;
     private final BervanLogger logger;
 
-    public CanvasService(CanvasRepository repository, HistoryCanvasRepository historyRepository, BervanLogger logger) {
+    public CanvasService(CanvasRepository repository,
+                         HistoryCanvasRepository historyRepository,
+                         SearchService searchService,
+                         BervanLogger logger) {
+        super(repository, searchService);
         this.repository = repository;
         this.historyRepository = historyRepository;
         this.logger = logger;
@@ -40,10 +43,6 @@ public class CanvasService implements BaseService<UUID, Canvas> {
         return new HashSet<>(repository.findByDeletedFalseAndOwnersId(AuthService.getLoggedUserId()));
     }
 
-    public Set<Canvas> loadForOwner(User user) {
-        return new HashSet<>(repository.findByDeletedFalseAndOwnersId(user.getId()));
-    }
-
     @PostFilter("(T(com.bervan.common.service.AuthService).hasAccess(filterObject.owners))")
     public List<Canvas> loadByName(String canvasName) {
         return repository.findByNameAndDeletedFalseAndOwnersId(canvasName, AuthService.getLoggedUserId());
@@ -60,11 +59,4 @@ public class CanvasService implements BaseService<UUID, Canvas> {
         return historyRepository.findAll();
     }
 
-    public void saveIfValid(List<? extends ExcelIEEntity> objects) {
-        List<? extends ExcelIEEntity> list = objects.stream().filter(e -> e instanceof Canvas).toList();
-        logger.debug("Filtered Pockets to be imported: " + list.size());
-        for (ExcelIEEntity excelIEEntity : list) {
-            repository.save(((Canvas) excelIEEntity));
-        }
-    }
 }
