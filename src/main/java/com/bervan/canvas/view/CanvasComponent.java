d1939beeb814c933dbf480b4058cdbe8089ca3ca
berluk12@gmail.com <berluk12@gmail.com>
2025-07-05 01:08:27 +0200

    Canvas refactored, Abstract Table and components refactored

--- src/main/java/com/bervan/canvas/view/CanvasComponent.java
+++ src/main/java/com/bervan/canvas/view/CanvasComponent.java
@@ -30,6 +30,7 @@ public class CanvasComponent extends AbstractBervanEntityView<UUID, Canvas> {
         super(new EmptyLayout(), service, Canvas.class);
         this.service = service;
         this.canvasEntity = canvasEntity;
+        renderCommonComponents();
         refresh();
     }
 
